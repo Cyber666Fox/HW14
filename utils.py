4c98fe87e73845d4c8fddde341080c87081e43de
Cyber666Fox <104504035+Cyber666Fox@users.noreply.github.com>
2022-06-04 18:32:14 +0800

    fixes

--- utils.py
+++ utils.py
@@ -21,13 +21,13 @@ def execute_query(query):
 
 def movie_by_title(title):
     db_conect = DbConnect('netflix.db')
-    db_conect.cur.execute(f"""SELECT title,country,release_year,genre,description FROM netflix WHERE title like '%{title}%' order by release_year desc limmit 1 ;""")
+    db_conect.cur.execute(f"""SELECT title,country,release_year,listed_in,description FROM netflix WHERE title like '%{title}%' order by release_year desc limit 1 ;""")
     result = db_conect.cur.fetchone()
-    return{"title":result[0], "country": result[1], "release_year": result[2],"genre":result[3],"description":result[4]}
+    return{"title":result[0], "country": result[1], "release_year": result[2],"listed_in":result[3],"description":result[4]}
 
 
 def movies_by_years(year1,year2):
-    query = f""" SELECT title,release_year FROM netflix WHERE release_year between '%{year1}%' and '%{year2}%' limit 100; """
+    query = f""" SELECT title,release_year FROM netflix WHERE release_year between '{year1}' and '{year2}' limit 100; """
     result = execute_query(query)
     result_list = []
     for movie in result:
@@ -43,14 +43,14 @@ def movies_by_rating(rating):
     result = execute_query(query)
     result_list = []
     for movie in result:
-        result_list.append({"title":{movie[0]},"rating": {movie[1]},"description":{movie[2]}})
+        result_list.append({"title":movie[0],"rating":movie[1],"description":movie[2]})
     return result_list
 
-def movies_by_ganre(ganre):
-    result = execute_query(f"""SELECT title, description FROM netflix WHERE listed_in like '%{ganre}%' order by release_year desc limit 10;""")
+def movies_by_genre(genre):
+    result = execute_query(f"""SELECT title, description FROM netflix WHERE listed_in like '%{genre}%' order by release_year desc limit 10;""")
     result_list = []
     for movie in result:
-        result_list.append({"title":{movie[0]},"description":{movie[1]}})
+        result_list.append({"title":movie[0],"description":movie[1]})
     return result_list
 
 def cast_partners (actor1, actor2):
@@ -66,10 +66,10 @@ def cast_partners (actor1, actor2):
             result_list.append(actor)
     return result_list
 
-def search_movie_by_param (movie_type, release_year, ganre):
-    query = f"""SELECT title, description FROM netflix WHERE type='{movie_type}'  AND release_year={release_year} AND listed_in LIKE '%{ganre}%'"""
+def search_movie_by_param (movie_type, release_year, genre):
+    query = f"""SELECT title, description FROM netflix WHERE type='{movie_type}'  AND release_year={release_year} AND listed_in LIKE '%{genre}%'"""
     result = execute_query(query)
     result_list = []
     for movie in result:
-        result_list.append({"title":{movie[0]},"description":{movie[1]}})
+        result_list.append({"title":movie[0],"description":movie[1]})
     return result_list
